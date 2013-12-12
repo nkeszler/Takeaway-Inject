34b1baa2338ccd7d07af53d0bad41ba36a9ffa4e
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-12-12 17:28:08 +0000

    feedback

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,6 +1,8 @@
 # Re-write of Inject method...
 module Enumerable
 	
+  # the argument should be optional
+  # also, why declare a block if you aren't using this variable?
 	def super_inject(init_val, &block)
 		memo = init_val
 		self.each {|x| memo = yield(memo ,x)}
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -14,4 +14,12 @@ describe Array do
 		expect(array.super_inject([],&block)).to eq(array.inject([]){block})
 	end
 
+  # Nicki, this will be a badge if you write more tests and fix the implementation
+
+  # test that it works as expected for an array of numbers
+  # and of strings (or something else)
+  # if the initial value is given
+  # and if it isn't
+  # Finally, test that the method doesn't modify the array it's called on
+
 end
